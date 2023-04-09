6397116d472adc986c9a046643d980daaaa1b29c
Aditya Sen <adityasen120@gmail.com>
2023-04-08 20:11:07 -0400

    add flask routing + pi serial code

---
+++ RaspberryPi-Server/server.py
@@ -0,0 +1,21 @@
+import serial
+from flask import Flask
+from flask import request  
+
+serialPort = serial.Serial('/dev/ttyACM0', 9600, timeout=1.0)
+
+serialPort.reset_input_buffer()
+serialPort.reset_output_buffer()
+
+app = Flask(__name__)  
+
+@app.route('/', methods=['POST'])
+def index():
+    if request.method == "POST":
+        data = request.form
+        name = data["name"]
+        preset_name = data["preset"]
+
+        serialPort.write(name)
+        serialPort.write(preset_name)
+
